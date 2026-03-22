6d0f3ef8851dee04c5e4d7e8441dff7ead92c3ca
ldevdantesl <buzurg.berdiev@icloud.com>
2026-03-22 00:57:37 -0400

    Initial Setup, add Masonry PodFile

--- GitHubBrowser/Controllers/MainViewController.m
+++ GitHubBrowser/Controllers/MainViewController.m
@@ -6,31 +6,48 @@
 //
 
 #import "MainViewController.h"
+#import <Masonry/Masonry.h>
+
+static const struct {
+    CGFloat spacing;
+    CGFloat padding;
+} Constants = {
+    .spacing = 10.0,
+    .padding = 16.0
+};
 
 @interface MainViewController ()
 
 @property (nonatomic, strong) UILabel *titleLabel;
-@property (nonatomic, assign) NSInteger counter;
 
 @end
 
 @implementation MainViewController
 
+- (UILabel *)titleLabel {
+    if (!_titleLabel) {
+        _titleLabel = [[UILabel alloc] init];
+        _titleLabel.text = @"First label";
+        _titleLabel.font = [UIFont systemFontOfSize:20];
+        _titleLabel.textColor = [UIColor whiteColor];
+        _titleLabel.textAlignment = NSTextAlignmentCenter;
+    }
+    return _titleLabel;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor systemBlueColor];
-    self.titleLabel = [[UILabel alloc] init];
+    [self setupUI];
 }
 
--(void)setupUI {
+- (void)setupUI {
+    [self.view addSubview:self.titleLabel];
     
-}
-
-- (void)configureLabel {
-    self.titleLabel.text = @"First label";
-    self.titleLabel.font = [UIFont systemFontOfSize:20];
-    self.titleLabel.textColor = [UIColor whiteColor];
-    self.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(self.view);
+        make.leading.trailing.equalTo(self.view).inset(10);
+    }];
 }
 
 @end
--- Podfile
+++ Podfile
@@ -1,10 +1,16 @@
-# Uncomment the next line to define a global platform for your project
-# platform :ios, '15.0'
+platform :ios, '15.0'
 
 target 'GitHubBrowser' do
-
-  use_frameworks!
+  inhibit_all_warnings!
   
-  pod "Masonry"
+  pod 'Masonry'
+  
+end
 
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '15.0'
+    end
+  end
 end
